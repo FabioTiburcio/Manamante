7c45ad348f6bebac8945ca3b4449d2228c075885
Fabio Tiburcio <49411361+FabioTiburcio@users.noreply.github.com>
2020-07-30 14:12:11 -0300

    Primeira cena criada

---
+++ Manamante/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D playerRb;
+    private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(movement);
+    }
+
+    void MovePlayer(Vector2 direction)
+    {
+
+    }
+}
